045c7f1b126ad4abd20b8c1b6e238fb98c6acabc
Ercanon <78048326+ercaon@users.noreply.github.com>
2025-06-17 20:40:08 +0200

    cleanup after lualine_components.lua removal

--- lua/custom/lualine_components.lua
+++
@@ -1,9 +0,0 @@
-local X = {}
-
-X.clock_pos = function()
-    local time = os.date("%H:%M")
-    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return string.format("%s | %d:%d", time, row, col + 1)
-end
-
-return X
