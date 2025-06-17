ff7139ce4298082b8734c1dbb86cdc22d9c6b836
Ercanon <78048326+ercaon@users.noreply.github.com>
2025-06-17 20:38:44 +0200

    lualine zum laufen gebracht

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,26 +1,30 @@
--- local clockrow_component = require("custom.lualine_components")
-local function clockpos()
-    local time = os.date("%H:%M")
-    local pos = vim.api.nvim_win_get_cursor(0)
-    local row, col = pos[1], pos[2]
-    return string.format("%s | %d:%d", time, row, col + 1)
-end
-
 return {
     {
         "nvim-lualine/lualine.nvim",
-		requires = {"nvim-lualine/nvim-web-devicons"},
-        options = {
-			icons_enabled = true,
-            theme = "onedark",
-        },
-		sections = {
-			lualine_a = {"mode"},
-			lualine_b = {"branch", "diff"},
-            lualine_c = {"filename", "filesize", "diagnostics"},
-			lualine_x = {"searchcount", "selectioncount"},
-            lualine_y = {"buffers", "tabs", "windows"},
-            lualine_z = {clockpos}
-		}
+        event = "VeryLazy",
+		requires = {"nvim-tree/nvim-web-devicons"},
+        config = function ()
+            local function clockpos()
+                local time = os.date("%H:%M")
+                local pos = vim.api.nvim_win_get_cursor(0)
+                local row, col = pos[1], pos[2]
+                return string.format("%s | %d:%d", time, row, col + 1)
+            end
+
+            require("lualine").setup({
+                options = {
+                    theme = "onedark",
+                    globalstatus = true
+                },
+		        sections = {
+		            lualine_a = {"mode"},
+			        lualine_b = {"branch", "diff"},
+                    lualine_c = {"filename"},
+			        lualine_x = {"selectioncount"},
+                    lualine_y = {"buffers"},
+                    lualine_z = {clockpos}
+		        }
+            })
+        end
     }
 }
