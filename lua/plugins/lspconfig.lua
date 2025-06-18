8db656731b7945f08697a6511451cfc54fc34416
Ercanon <78048326+ercaon@users.noreply.github.com>
2025-06-18 16:28:38 +0200

    fixed typos_lsp toggel functionality

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,37 +1,4 @@
---[[
-local lspconfig = require("lspconfig")
-local typos_active = true
 
-local function is_buffer_loaded(bufnr)
-    return vim.api.nvim_buf_is_loaded(bufnr)
-end
-
-function ToggleTypos()
-    local bufnr = vim.api.nvim_get_current_buf()
-
-    if not is_buffer_loaded(bufnr) then
-        print("Buffer is not loaded!")
-        return
-    end
-
-    local client_id = vim.lsp.get_active_clients({name = "typos_lsp"})[1]
-
-    if typos_active then
-        -- Stop the LSP client
-        if client_id then
-            vim.lsp.buf_detach_client(bufnr, client_id.id)
-            typos_active = false
-            print("Typos disabled.")
-        end
-    else
-        -- Start the LSP client
-        lspconfig.typos_lsp.setup({})
-        vim.lsp.buf_attach_client(bufnr, client_id.id)
-        typos_active = true
-        print("Typos enabled.")
-    end
-end
---]]
 return {
 	-- Mason
     {
@@ -44,12 +11,52 @@ return {
         "mason-org/mason-lspconfig.nvim",
 	    dependencies = {"neovim/nvim-lspconfig"},
         config = function()
+            local lspconfig = require("lspconfig")
 		    require("mason-lspconfig").setup({
-                ensure_installed = {"lua_ls"},
+                ensure_installed = {"lua_ls", "typos_lsp"},
                 automatic_enable = true
             })
+
+            -- lua_ls setup
             vim.lsp.config("lua_ls", {settings = {Lua = {diagnostics = {globals = {"vim"}}}}})
-            -- vim.keymap.set("n", "<leader>tsc", ToggleTypos, {noremap = true, silent = true, desc = "Toggle Spellchecking"})
+
+
+            -- typos_ls setup
+            lspconfig.typos_lsp.setup({})
+            local typos_active = true
+
+            function ToggleTypos()
+                local bufnr = vim.api.nvim_get_current_buf()
+
+                if not vim.api.nvim_buf_is_loaded(bufnr) then
+                    print("Buffer is not loaded!")
+                    return
+                end
+
+                local client = vim.lsp.get_active_clients({name = "typos_lsp"})[1]
+
+                if typos_active then
+                    -- Stop the LSP client
+                    if client then
+                        vim.lsp.buf_detach_client(bufnr, client.id)
+                        typos_active = false
+                        print("Typos disabled.")
+                    end
+                else
+                    -- Start the LSP client
+                    if client then
+                        vim.lsp.buf_attach_client(bufnr, client.id)
+                        typos_active = true
+                        print("Typos enabled.")
+                    else
+                        lspconfig.typos_lsp.setup({})
+                        typos_active = true
+                        print("Typos configured & enabled.")
+                    end
+                end
+            end
+
+            vim.keymap.set("n", "<leader>tsc", ToggleTypos, {noremap = true, silent = true, desc = "Toggle Spellchecking"})
         end,
     }
 }
